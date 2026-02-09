33475cfb8423322e0518f5dfd1ee035750bca94d
AshDashTooFast3 <338127@student.mboutrecht.nl>
2026-02-09 16:58:27 +0100

    Les 1 en 2 af: CRUD opbouwen

---
+++ app/Http/Controllers/CategorieController.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace App\Http\Controllers;
+use App\Http\Resources\CategorieResource;
+use App\Models\Categorie;
+use App\Http\Requests\StoreCategorieRequest;
+use App\Http\Requests\UpdateCategorieRequest;
+class CategorieController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        return CategorieResource::collection(Categorie::all());
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreCategorieRequest $request)
+    {
+        return new CategorieResource(Categorie::create($request->validated()));
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Categorie $categorie)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateCategorieRequest $request, Categorie $categorie)
+    {
+        $categorie->update($request->validated());
+ 
+        return new CategorieResource($categorie);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Categorie $categorie)
+    {
+        $categorie->delete();
+ 
+        return response()->noContent();
+    }
+}
