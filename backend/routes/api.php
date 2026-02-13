487bed443a2e3cd7e10e58b3b5706355e24edd43
AshDashTooFast3 <338127@student.mboutrecht.nl>
2026-02-13 13:55:40 +0100

    Juiste apiResource gebruiken

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -9,7 +9,10 @@ Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
 
-Route::apiResource('transactions', TransactionController::class);
-Route::get('/categories', [CategoryController::class, 'index']);
-Route::get('/categories/{category}', [CategoryController::class, 'show']); 
+// ...
  
+//Route::group(['middleware' => 'auth:sanctum'], function () {
+    Route::apiResource('categories', CategoryController::class);
+    Route::apiResource('transactions', TransactionController::class);
+//});
+
